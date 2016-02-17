8d11f7185f6e49a410a591f672758381b9539241
Daniel Ferraz <d.ferrazm@gmail.com>
2016-02-17 18:28:07 +0100

    Add page to edit a movie

--- spec/models/users/movies_collection_spec.rb
+++ spec/models/users/movies_collection_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe Users::MoviesCollection do
-  let(:user) { create :user }
-  let(:collection) { described_class.new(user) }
+  let(:user) { build_stubbed :user }
+  let(:matrix) { create :movie, user: user, name: 'Matrix' }
+  let(:collection) { described_class.new(user.movies) }
 
   describe '#add' do
     it 'adds a new movie to the users collection' do
@@ -12,6 +13,22 @@ describe Users::MoviesCollection do
     it 'returns the movie with errors in case of failure' do
       movie = collection.add(attributes_for :movie, name: nil)
       expect(movie.errors).to_not be_empty
+    end
+  end
+
+  describe '#find' do
+    subject(:movie) { collection.find(matrix.id) }
+
+    it 'finds the movie by id and returns it' do
+      expect(movie).to eq matrix
+    end
+  end
+
+  describe '#update_movie' do
+    subject(:movie) { collection.update_movie(matrix.id, name: 'Matrix Reloaded') }
+
+    it 'updates the movie with the new params and returns it' do
+      expect(movie.name).to eq 'Matrix Reloaded'
     end
   end
 end
